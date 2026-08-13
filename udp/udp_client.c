a2166dde4204da006e66239ff99c0e6aa00143e5
czelyk <ahmethassancelik@gmail.com>
2026-08-13 14:10:12 +0200

    feed: udp_server and udp_client integrated.

---
+++ udp/udp_client.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define SERVER_IP "127.0.0.1"
+#define SERVER_PORT 5000
+
+
+int main(void)
+{
+    int sockfd;
+    struct sockaddr_in server_addr;
+    const char *message = "merhaba";
+    ssize_t sent_bytes;
+
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+
+    if(sockfd < 0)
+    {
+        perror("socket");
+        return EXIT_FAILURE;
+    }
+
+    memset(&server_addr, 0, sizeof(server_addr));
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(SERVER_PORT);
+
+    if (inet_pton(AF_INET,
+                SERVER_IP,
+                &server_addr.sin_addr) != 1)
+    {
+        perror("inet_pton");
+        close(sockfd);
+        return EXIT_FAILURE;
+    }
+
+    sent_bytes = sendto(
+        sockfd,
+        message,
+        strlen(message),
+        0,
+        (struct sockaddr *)&server_addr,
+        sizeof(server_addr)
+    );
+
+    if(sent_bytes < 0)
+    {
+        perror("sendto");
+        close(sockfd);
+        return EXIT_FAILURE;
+    }
+
+    printf("Sent %zd bytes to %s:%d\n",
+            sent_bytes,
+            SERVER_IP,
+            SERVER_PORT);
+
+    close(sockfd);
+    return 0;
+}
--- udp/udp_server.c
+++ udp/udp_server.c
@@ -14,6 +14,13 @@ int main(void)
 {
     int sockfd;
     struct sockaddr_in server_addr;
+    char buffer[BUFFER_SIZE];
+
+    struct sockaddr_in client_addr;
+    socklen_t client_addr_len;
+    ssize_t received_bytes;
+
+    unsigned short client_port;
 
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
 
@@ -37,6 +44,51 @@ int main(void)
         close(sockfd);
         return EXIT_FAILURE;
     }
+
+    printf("UDP server listening on port %d...\n", PORT);
+    client_addr_len=sizeof(client_addr);
+
+    received_bytes = recvfrom(
+        sockfd,
+        buffer,
+        sizeof(buffer) -1,
+        0,
+        (struct sockaddr *)&client_addr,
+        &client_addr_len
+    );
+
+    if(received_bytes < 0)
+    {
+        perror("recvfrom");
+        close(sockfd);
+        return EXIT_FAILURE;
+    }
+
+    buffer[received_bytes] = '\0';
+
+
+    client_port = ntohs(client_addr.sin_port);
+
+
+    char client_ip[INET_ADDRSTRLEN];
+
+    if(inet_ntop(AF_INET,
+                &client_addr.sin_addr,
+                client_ip,
+                sizeof(client_ip)) == NULL)
+    {
+        perror("inet_ntop");
+        close(sockfd);
+        return EXIT_FAILURE;
+    }
+
+    printf("Received %zd bytes from %s:%u: %s\n",
+        received_bytes,
+        client_ip,
+        client_port,
+        buffer);
+
+    close(sockfd);
 
     return 0;
 }
