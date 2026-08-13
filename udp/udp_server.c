6dca2aac0b1d3142c2c564f1a9ef3cde84223991
czelyk <ahmethassancelik@gmail.com>
2026-08-13 10:30:11 +0200

    implement basic UDP server setup with socket creation and binding

--- udp/udp_server.c
+++ udp/udp_server.c
@@ -1,1 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define PORT 5000
+#define BUFFER_SIZE 1024
+
+int main(void)
+{
+    int sockfd;
+    struct sockaddr_in server_addr;
+
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+
+    if(sockfd < 0)
+    {
+        perror("socket");
+        return EXIT_FAILURE;
+    }
+
+    memset(&server_addr, 0, sizeof(server_addr));
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    if(bind(sockfd,
+            (struct sockaddr *)&server_addr,
+            sizeof(server_addr)) < 0)
+    {
+        perror("bind");
+        close(sockfd);
+        return EXIT_FAILURE;
+    }
+
+    return 0;
+}
